e3b46d2c1b328cc3bb2bb074b7e27c0155ec1cc7
Nejla Şahin <2017123027@cumhuriyet.edu.tr>
2021-12-18 12:55:16 +0300

    Initial commit

---
+++ src/main/java/com/nejlasahin/springshortenedurl/SpringShortenedUrlApplication.java
@@ -0,0 +1,13 @@
+package com.nejlasahin.springshortenedurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShortenedUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShortenedUrlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nejlasahin/springshortenedurl/SpringShortenedUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nejlasahin.springshortenedurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShortenedUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
